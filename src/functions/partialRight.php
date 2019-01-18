48f290741137f3a0777daf63ae70dfa55ab69f16
M4RC3L0 <joaobat@live.com.pt>
2019-01-18 12:05:13 +0000

    fix partialRight

--- src/functions/partialRight.php
+++ src/functions/partialRight.php
@@ -6,7 +6,7 @@ function partialRight($fn)
 {
     return function ($lastArgs) use ($fn) {
         return function (...$firsts) use ($fn, $lastArgs) {
-            return \call_user_func_array($fn, \array_combine($firsts, $lastArgs));
+            return \call_user_func_array($fn, \array_merge($firsts, $lastArgs));
         };
     };
 }
