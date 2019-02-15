4c002a8ec037ab7fa46a550627f50109f95f00ef
M4RC3L0 <joaobat@live.com.pt>
2019-02-15 19:39:16 +0000

    add docks
    Signed-off-by: M4RC3L0 <joaobat@live.com.pt>

--- src/functions/useWith.php
+++ src/functions/useWith.php
@@ -4,7 +4,21 @@ namespace FPPHP\Functions;
 
 use function FPPHP\Lists\map;
 
-
+/**
+ *
+ * useWith: ((x1, x2, …) → z) → [(a → x1), (b → x2), …] → (a → b → … → z)
+ *
+ * Recieves a function and a list of trasformers and returns a function
+ * with the same arity of the provided function and returns teh result
+ * of calling the provided functions with the args trasformed by is
+ * curresponding trasformer (by index)
+ *
+ * @param callable $fn
+ * @param array $trasformers
+ * @param mixed ...$args
+ * @return mixed
+ *
+ */
 function useWith(callable $fn)
 {
     return function (array $trasformers) use ($fn) {
